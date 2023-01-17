96a641c050289ba94cd2b6633011aeceb8aba489
okoro Jeremiahm <okorojeremiah91@gmail.com>
2023-01-17 16:15:53 +0100

    added repositories

--- src/main/java/com/book/amazingbookseller/UserRepository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.book.amazingbookseller.UserRepository;
-
-import com.book.amazingbookseller.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
